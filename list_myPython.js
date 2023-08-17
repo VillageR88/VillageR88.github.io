d45faf7680bb967afbc99ed7558658ffae342538
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-17 19:45:55 +0200

    Create list_myPython.js

---
+++ list_myPython.js
@@ -0,0 +1,23 @@
+async function displayFileContent() {
+const response = await fetch("/myPython.txt");
+const text = await response.text();
+const lines = text.split('\n');
+
+const fileContentElement = document.getElementById('fileContent3');
+
+lines.forEach((line) => {
+    const p = document.createElement('p');
+    const a = document.createElement('a');
+
+    // Utwórz link do pliku/katalogu
+    const link = `https://github.com/VillageR88/Codewars/tree/main/Python/${line}`; // Zastąp ścieżką do katalogu
+
+    a.href = link;
+    a.textContent = `1.1.`+ line.slice(0,6).split(/^0+/g).join("")+line.slice(6) // Tekst linku
+    
+    // Dodaj link do paragrafu i paragraf do elementu fileContent
+    p.appendChild(a);
+    fileContentElement.appendChild(p);
+    });
+}
+    displayFileContent();
