d7128ca3411fd850a71553692c58392c32daad04
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-17 12:53:15 +0200

    Create script_myTrueScript.js

---
+++ script_myTrueScript.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+fs.readdir('C:/Users/Karol/Documents/GitHub/Codewars/TypeScript', (err, files) => {
+  if (err) {
+    console.error('Error reading directory:', err);
+    return;
+  }
+
+  // Zapisz listę plików do pliku filesList.txt
+  fs.writeFile('myTrueScript.txt', files.join('\n'), (writeErr) => {
+    if (writeErr) {
+      console.error('Error writing to file:', writeErr);
+      return;
+    }
+    console.log('List of files saved to myTrueScript.txt');
+  });
+});
