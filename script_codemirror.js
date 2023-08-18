f4071f82928753b4695b35f17074767ea00f71f1
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-18 19:52:06 +0200

    upd

--- script_codemirror.js
+++ script_codemirror.js
@@ -2,7 +2,7 @@ var editor = CodeMirror.fromTextArea(document.getElementById("codeEditor"), {
     lineNumbers: true,
     mode: "javascript",
     lineWrapping: true,
-    readOnly: true,
+    readOnly: false,
     theme: "twilight",
 });
 
