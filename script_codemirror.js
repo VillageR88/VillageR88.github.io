6a44072a5b1c94f990f44cb372611eb41b48b42d
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-18 13:47:23 +0200

    update

---
+++ script_codemirror.js
@@ -0,0 +1,32 @@
+var editor = CodeMirror.fromTextArea(document.getElementById("codeEditor"), {
+    lineNumbers: true,
+    mode: "javascript",
+    lineWrapping: true,
+    readOnly: true,
+    theme: "twilight",
+});
+var runButton = document.getElementById("runButton");
+var outputContainer = document.getElementById("outputContainer");
+runButton.addEventListener("click", function () {
+    try {
+        var code = editor.getValue();
+        var oldConsoleLog = console.log;
+        var output = "";
+        console.log = function (message) {
+            output += message + "\n";
+        };
+        eval(code);
+        console.log = oldConsoleLog;
+        outputContainer.textContent = output;
+    } catch (error) {
+        outputContainer.textContent = "Error: " + error.message;
+    }
+});
+
+// Pobierz zawartość z linku i wstaw ją do edytora
+fetch('https://raw.githubusercontent.com/VillageR88/Codewars/main/JavaScript/000011.%20Calculate%20BMI.js')
+    .then(response => response.text())
+    .then(code => {
+        editor.setValue(code);
+    })
+    .catch(error => console.error('Błąd:', error));
