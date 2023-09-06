7297dd5693fd73701e814d28fa17aec3583fbcd1
VillageR88 <villager88pl@gmail.com>
2023-09-07 00:58:11 +0200

    commit

--- Scripts/settings.js
+++ Scripts/settings.js
@@ -3,10 +3,20 @@ localStorage.getItem("theme1");
 if (localStorage.getItem("theme1") == "light") {
     theme1.textContent = "Light theme";
     theme1.classList.add('enhanced');
+    wrapper0.classList.add('enhanced');
+    wrapper1.classList.add('enhanced');
+    center.classList.add('enhanced');
+    document.body.classList.add('enhanced');
+    document.querySelectorAll('a').forEach(link => link.classList.add('enhanced'));
   } else {
     theme1.textContent = "Dark theme";
     localStorage.setItem('theme1', 'dark');
     theme1.classList.remove('enhanced');
+    wrapper0.classList.remove('enhanced');
+    wrapper1.classList.remove('enhanced');
+    center.classList.remove('enhanced');
+    document.body.classList.remove('enhanced');
+    document.querySelectorAll('a').forEach(link => link.classList.remove('enhanced'));
   }
 
 document.getElementById('theme1').addEventListener('click', function() {
@@ -14,9 +24,20 @@ document.getElementById('theme1').addEventListener('click', function() {
     theme1.textContent = "Light theme";
     localStorage.setItem('theme1', 'light');
     theme1.classList.add('enhanced');
+    wrapper0.classList.add('enhanced');
+    wrapper1.classList.add('enhanced');
+    center.classList.add('enhanced');
+    document.body.classList.add('enhanced');
+    document.querySelectorAll('a').forEach(link => link.classList.add('enhanced'));
+
   } else {
     theme1.textContent = "Dark theme";
     localStorage.setItem('theme1', 'dark');
     theme1.classList.remove('enhanced');
+    wrapper0.classList.remove('enhanced');
+    wrapper1.classList.remove('enhanced');
+    center.classList.remove('enhanced');
+    document.body.classList.remove('enhanced');
+    document.querySelectorAll('a').forEach(link => link.classList.remove('enhanced'));
   }
 });
