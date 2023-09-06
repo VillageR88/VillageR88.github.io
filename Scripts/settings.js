bf8f26ef45e0b770c57ed863edc663988d375cae
VillageR88 <villager88pl@gmail.com>
2023-09-06 23:49:29 +0200

    commit

--- Scripts/settings.js
+++ Scripts/settings.js
@@ -4,14 +4,10 @@ document.getElementById('theme1').addEventListener('click', function() {
   if (localStorage.getItem("theme1") == "dark") {
     theme1.textContent = "Light theme";
     localStorage.setItem('theme1', 'light');
-    theme1.style.backgroundColor = 'white';
-    theme1.style.color = 'black';
     theme1.classList.add('enhanced');
   } else {
     theme1.textContent = "Dark theme";
     localStorage.setItem('theme1', 'dark');
-    theme1.style.backgroundColor = 'black';
-    theme1.style.color = 'white';
     theme1.classList.remove('enhanced');
   }
 });
