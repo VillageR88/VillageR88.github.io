9334ab7100e676c7954bbf46e14d39a2b870c991
VillageR88 <villager88pl@gmail.com>
2023-09-08 11:35:16 +0200

    commit

--- Scripts/settings.js
+++ Scripts/settings.js
@@ -8,7 +8,6 @@ if (localStorage.getItem("theme1") == "light") {
     editor1.setOption();
     theme1.textContent = "Light theme";
     theme1.classList.add('enhanced');
-    wrapper0.classList.add('enhanced');
     wrapper1.classList.add('enhanced');
     center.classList.add('enhanced');
     document.body.classList.add('enhanced');
@@ -22,7 +21,6 @@ if (localStorage.getItem("theme1") == "light") {
     theme1.textContent = "Dark theme";
     localStorage.setItem('theme1', 'dark');
     theme1.classList.remove('enhanced');
-    wrapper0.classList.remove('enhanced');
     wrapper1.classList.remove('enhanced');
     center.classList.remove('enhanced');
     document.body.classList.remove('enhanced');
@@ -38,7 +36,6 @@ document.getElementById('theme1').addEventListener('click', function() {
     theme1.textContent = "Light theme";
     localStorage.setItem('theme1', 'light');
     theme1.classList.add('enhanced');
-    wrapper0.classList.add('enhanced');
     wrapper1.classList.add('enhanced');
     center.classList.add('enhanced');
     document.body.classList.add('enhanced');
@@ -54,7 +51,6 @@ document.getElementById('theme1').addEventListener('click', function() {
     theme1.textContent = "Dark theme";
     localStorage.setItem('theme1', 'dark');
     theme1.classList.remove('enhanced');
-    wrapper0.classList.remove('enhanced');
     wrapper1.classList.remove('enhanced');
     center.classList.remove('enhanced');
     document.body.classList.remove('enhanced');
