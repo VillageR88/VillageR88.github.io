7af3b0781474024a6c334ea285796a1cc8ae2358
VillageR88 <villager88pl@gmail.com>
2023-09-13 21:31:09 +0200

    commit

--- Scripts/sidebar.js
+++ Scripts/sidebar.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     function toggleNav() {
-        if (document.querySelector('.sidebar').style.width === '250px') {
+        if (document.querySelector('.sidebar').style.width === '340px') {
             document.querySelector('.sidebar').style.width = '0';
         } else {
-            document.querySelector('.sidebar').style.width = '250px';
+            document.querySelector('.sidebar').style.width = '340px';
         }
     }
     document.querySelector('.togglebtn').addEventListener('click', toggleNav);
