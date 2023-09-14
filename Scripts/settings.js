2c9bd288acf0978d5c3b961c9a33bab51741177c
VillageR88 <villager88pl@gmail.com>
2023-09-14 13:35:11 +0200

    commit

--- Scripts/settings.js
+++ Scripts/settings.js
@@ -1,16 +1,19 @@
 var light = "default";
 var dark = "twilight";
+const mediaQuery = window.matchMedia("(max-height: 500px)");
 var customized = "customized"
 if (localStorage.getItem("theme1") == dark) {
   editor1.setOption("theme", dark);
   editor2.setOption("theme", dark);
   theme1.textContent = "Dark theme";
+  if (mediaQuery.matches) theme1.textContent = theme1.textContent.slice(0, -6);  
   localStorage.setItem('theme1', dark);
 }
 else if (localStorage.getItem("theme1") == light) {
   editor1.setOption("theme", light);
   editor2.setOption("theme", light);
   theme1.textContent = "Light theme";
+  if (mediaQuery.matches) theme1.textContent = theme1.textContent.slice(0, -6);  
   theme1.classList.add('enhanced');
   debugButton.classList.add('enhanced');
   runButton.classList.add('enhanced');
@@ -34,6 +37,7 @@ else if (localStorage.getItem("theme1") == customized) {
   editor1.setOption("theme", customized);
   editor2.setOption("theme", customized);
   theme1.textContent = "Custom theme";
+  if (mediaQuery.matches) theme1.textContent = theme1.textContent.slice(0, -6);  
   theme1.classList.add('customized');
   debugButton.classList.add('customized');
   document.querySelector('.togglebtn').classList.add('customized');
@@ -60,6 +64,7 @@ document.getElementById('theme1').addEventListener('click', function () {
     editor1.setOption("theme", light);
     editor2.setOption("theme", light);
     theme1.textContent = "Light theme";
+    if (mediaQuery.matches) theme1.textContent = theme1.textContent.slice(0, -6);  
     localStorage.setItem('theme1', light);
     theme1.classList.add('enhanced');
     debugButton.classList.add('enhanced');
@@ -90,6 +95,7 @@ document.getElementById('theme1').addEventListener('click', function () {
     editor1.setOption("theme", customized);
     editor2.setOption("theme", customized);
     theme1.textContent = "Custom theme";
+    if (mediaQuery.matches) theme1.textContent = theme1.textContent.slice(0, -6);  
     localStorage.setItem('theme1', customized);
     theme1.classList.add('customized');
     debugButton.classList.add('customized');
@@ -121,6 +127,7 @@ document.getElementById('theme1').addEventListener('click', function () {
     editor1.setOption("theme", dark);
     editor2.setOption("theme", dark);
     theme1.textContent = "Dark theme";
+    if (mediaQuery.matches) theme1.textContent = theme1.textContent.slice(0, -6);  
     localStorage.setItem('theme1', dark);
     theme1.classList.remove('enhanced', 'customized');
     debugButton.classList.remove('enhanced', 'customized');
