f19fc03f8683894134e084acacc811662af66715
VillageR88 <villager88pl@gmail.com>
2023-09-17 22:51:53 +0200

    commit

--- Scripts/my_component.jsx
+++ Scripts/my_component.jsx
@@ -1,22 +1,2 @@
-class MyButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isLiked: false };
-  }
-
-  render() {
-    if (this.state.isLiked) {
-      return <p>Yes, I Really Like This.</p>;
-    }
-
-    return (
-      <button onClick={() => this.setState({ isLiked: true })}>
-        Like Button
-      </button>
-    );
-  }
-}
-
-const domContainer = document.querySelector('#some_random_id');
-
-ReactDOM.render(<MyButton />, domContainer);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello, world!</h1>);
