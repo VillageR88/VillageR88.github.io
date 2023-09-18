cc32bf88d325dc401c989ecf0178125e8283a26a
VillageR88 <villager88pl@gmail.com>
2023-09-18 13:21:51 +0200

    commit

--- Scripts/my_component.jsx
+++ Scripts/my_component.jsx
@@ -21,14 +21,16 @@ class Counter extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello, react!</h1>
-                <h1 id="counter">{this.state.counter}</h1>
-                <button style={{ backgroundColor: 'red' }} type="button" id="decrement" onClick={this.handleDecrement}>
+                <h1 style={{ backgroundColor: "cornflowerblue", padding: 30, display: "flex", justifyContent: "center"}}>Hello, react! I'm just messing around.</h1>
+                <h1 style={{border: "solid", padding: 10, display: "flex", justifyContent: "end"}} id="counter">{this.state.counter}</h1>
+                <td style={{display: "flex", justifyContent:"center",}}>
+                <button style={{ backgroundColor: "red", padding: 30 }} type="button" id="decrement" onClick={this.handleDecrement}>
                     Decrement
                 </button>
-                <button style={{ backgroundColor: 'lime' }} type="button" id="increment" onClick={this.handleIncrement}>
+                <button style={{ backgroundColor: "lime", padding: 30 }} type="button" id="increment" onClick={this.handleIncrement}>
                     Increment
                 </button>
+                </td>
             </div>
         );
     }
