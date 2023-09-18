bbd6d57e2cd85069883ccb276f8b7b5069264f60
VillageR88 <villager88pl@gmail.com>
2023-09-18 07:29:56 +0200

    comment

--- Scripts/my_component.jsx
+++ Scripts/my_component.jsx
@@ -1,6 +1,39 @@
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <div>
-        <h1>Hello, react!</h1>
-    </div>
-);
+class Counter extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        counter: 0,
+      };
+    }
+  
+    handleIncrement = () => {
+      this.setState((prevState) => ({
+        counter: prevState.counter + 1,
+      }));
+    }
+  
+    handleDecrement = () => {
+      this.setState((prevState) => ({
+        counter: prevState.counter - 1,
+      }));
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1 id="counter">{this.state.counter}</h1>
+          <button type="button" id="decrement" onClick={this.handleDecrement}>
+            Decrement
+          </button>
+          <button type="button" id="increment" onClick={this.handleIncrement}>
+            Increment
+          </button>
+        </div>
+      );
+    }
+  }
+  
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<Counter />);
+  
