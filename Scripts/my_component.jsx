6d1d6fa2af68885f8ec343abc7976421f9802ef7
VillageR88 <villager88pl@gmail.com>
2023-09-18 21:59:30 +0200

    commit

--- Scripts/my_component.jsx
+++ Scripts/my_component.jsx
@@ -1,3 +1,8 @@
+var light = "default";
+var dark = "twilight";
+var customized = "customized";
+
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -5,6 +10,27 @@ class App extends React.Component {
             counter: 0,
             nowTime: new Date().toLocaleTimeString(),
         };
+    }
+
+    light = () => {
+        theme1.textContent = "☀";
+        localStorage.setItem('theme1', light);
+        document.body.classList.add('enhanced');
+        theme1.classList.add('enhanced');
+    }
+
+    dark = () => {
+        theme1.textContent = "☽";
+        localStorage.setItem('theme1', dark);
+        document.body.classList.remove('enhanced', 'customized');
+        theme1.classList.remove('enhanced', 'customized');
+    }
+
+    customized = () => {
+        theme1.textContent = "✰";
+        localStorage.setItem('theme1', customized);
+        document.body.classList.add('customized');
+        theme1.classList.add('customized');
     }
 
     handleZeroing = () => {
@@ -25,6 +51,22 @@ class App extends React.Component {
         }));
     }
 
+    handleTheme = () => {
+        const theme = localStorage.getItem("theme1");
+
+        if (theme === dark) {
+            this.light();
+        } else if (theme === light) {
+            this.customized();
+        } else if (theme === customized) {
+            this.dark();
+        }
+    };
+
+    handleExit = () => {
+        window.location.href = 'index.html';
+    };
+
     componentDidMount() {
         //document.body.style.backgroundColor = 'lightblue';
         this.timer = setInterval(() => {
@@ -32,6 +74,16 @@ class App extends React.Component {
                 nowTime: new Date().toLocaleTimeString(),
             });
         }, 1000);
+
+        const theme = localStorage.getItem("theme1");
+
+        if (theme === light) {
+            this.light();
+        } else if (theme === dark) {
+            this.dark();
+        } else if (theme === customized) {
+            this.customized();
+        }
     }
 
     componentWillUnmount() {
@@ -42,12 +94,10 @@ class App extends React.Component {
     render() {
         const nowTime = new Date().toLocaleTimeString();
         return (
-
             <div>
                 <tb style={{
                     display: "flex",
                     alignItems: "center",
-
                 }}>
                     <h2 style={{
                         display: "flex",
@@ -60,23 +110,33 @@ class App extends React.Component {
                         {nowTime}
                     </h2>
                     <h1 style={{
-                        fontSize: 40,
+                        fontSize: 30,
                         //textShadow: "1px 0px 0.1px pink",
-                        color: "black",
-                        padding: "10px",
+                        //color: "black",
                         display: "flex",
                         flex: 4,
-                        justifyContent: "center"
+                        justifyContent: "center",
+                        color: "inherit"
                     }}>
                         Hello, React 💪 I'm just messing around. 🐒
                     </h1>
+                    <button style={{
+                        marginLeft: "3px"
+                    }} type="button" id="theme1" onClick={this.handleTheme}>Theme</button>
+                    <button style={{
+                        fontSize: "20px",
+                        border: "solid",
+                        padding: "10px",
+                        marginRight: "10px"
+                    }} type="button" id="exitReact" onClick={this.handleExit}>Exit React</button>
                 </tb>
-                <h1 style={{
-                    border: "solid",
-                    padding: 10,
+                <div style={{
+                    fontSize: "30px",
+                    padding: "10px",
                     display: "flex",
-                    justifyContent: "end"
-                }} id="counter">{this.state.counter}</h1>
+                    justifyContent: "center",
+
+                }} id="counter">{this.state.counter}</div>
                 <div style={{
                     display: "flex",
                     justifyContent: "center",
@@ -84,27 +144,27 @@ class App extends React.Component {
                     <button style={{
                         fontWeight: "bold",
                         textShadow: "1px 1px 1px white",
-                        fontSize: 22,
+                        fontSize: "18px",
                         backgroundColor: "lightgrey",
-                        padding: 20
+                        padding: "10px",
                     }} type="button" id="zero" onClick={this.handleZeroing}>
                         Zeroing
                     </button>
                     <button style={{
                         fontWeight: "bold",
                         textShadow: "1px 1px 1px white",
-                        fontSize: 22,
+                        fontSize: "18px",
                         backgroundColor: "lightcoral",
-                        padding: 20
+                        padding: "10px",
                     }} type="button" id="decrement" onClick={this.handleDecrement}>
                         Decrement
                     </button>
                     <button style={{
                         fontWeight: "bold",
                         textShadow: "1px 1px 1px white",
-                        fontSize: 22,
+                        fontSize: "18px",
                         backgroundColor: "lightgreen",
-                        padding: 20
+                        padding: "10px",
                     }} type="button" id="increment" onClick={this.handleIncrement}>
                         Increment
                     </button>
