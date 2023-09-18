33e6b9603b2bd28f7344671355384f4b8146c65b
VillageR88 <villager88pl@gmail.com>
2023-09-18 07:32:11 +0200

    commit

--- Scripts/my_component.jsx
+++ Scripts/my_component.jsx
@@ -1,39 +1,39 @@
 class Counter extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        counter: 0,
-      };
+        super(props);
+        this.state = {
+            counter: 0,
+        };
     }
-  
-    handleIncrement = () => {
-      this.setState((prevState) => ({
-        counter: prevState.counter + 1,
-      }));
-    }
-  
-    handleDecrement = () => {
-      this.setState((prevState) => ({
-        counter: prevState.counter - 1,
-      }));
-    }
-  
-    render() {
-      return (
-        <div>
-          <h1 id="counter">{this.state.counter}</h1>
-          <button type="button" id="decrement" onClick={this.handleDecrement}>
-            Decrement
-          </button>
-          <button type="button" id="increment" onClick={this.handleIncrement}>
-            Increment
-          </button>
-        </div>
-      );
-    }
-  }
-  
 
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(<Counter />);
-  
+    handleIncrement = () => {
+        this.setState((prevState) => ({
+            counter: prevState.counter + 1,
+        }));
+    }
+
+    handleDecrement = () => {
+        this.setState((prevState) => ({
+            counter: prevState.counter - 1,
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, react!</h1>
+                <h1 id="counter">{this.state.counter}</h1>
+                <button type="button" id="decrement" onClick={this.handleDecrement}>
+                    Decrement
+                </button>
+                <button type="button" id="increment" onClick={this.handleIncrement}>
+                    Increment
+                </button>
+            </div>
+        );
+    }
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Counter />);
