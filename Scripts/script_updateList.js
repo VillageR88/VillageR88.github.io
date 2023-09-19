888db3c91b483c335010fd9f662db1f93a719be3
VillageR88 <villager88pl@gmail.com>
2023-09-19 14:20:32 +0200

    commit

--- Scripts/script_updateList.js
+++ Scripts/script_updateList.js
@@ -1,9 +1,6 @@
-if (window.location.protocol === 'file:') {
-    // This script will only run when the site is loaded locally
 require('./script_myTypeScript.js');
 require('./script_myJavaScript.js');
 require('./script_myPython.js');
 require('./script_myCSharp.js');
 require('./script_myReact.js');
 alert("This script is running locally!");
-}
